c386436089b1ca7ebece5c8a51c10bbe378e6865
2327851 <59999314+2327851@users.noreply.github.com>
2020-03-31 19:36:21 +0300

    deleted file accidentally added in wrong place

--- mystays_project/mystays/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from mystays.models import Stay, Review, UserProfile
-
-class StayAdmin(admin.ModelAdmin):
-    prepopulated_fields = {'slug':('name',)}
-
-admin.site.register(Stay, StayAdmin)
-admin.site.register(Review)
-admin.site.register(UserProfile)
